2321ef036068552b293191e8ecc1e521a6d1f6ce
Master-M <145254775+GrinenkoIvan@users.noreply.github.com>
2024-02-18 19:20:26 +0300

    Lesson 24.12.2023

--- main.py
+++ main.py
@@ -1235,7 +1235,7 @@
 #     print(i)
 
 # r = ['ab_1', 'ac_2', 'bc_1', 'bc_2']
- # a = [i for i in r if 'a' not in i]
+# a = [i for i in r if 'a' not in i]
 # a = ['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r]
 # a = ['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r if i[1] == 'c']
 # print(a)
@@ -3504,62 +3504,8 @@
 
 #
 # from typing import Union
-#
-#
-# def fill_data() -> list:
-#     """Заполняет базу данных студентов"""
-#     global students_count
-#     # Ввод пользователем количества студентов.
-#     students_count = int(input('Количество студентов: '))
-#     # Заполнение базы студентов.
-#     data: list[dict[str: Union[str, int]]] = [
-#         {
-#             'id': i,
-#             'Имя': input(f'Имя {i}-го студента: '),
-#             'Фамилия': input(f'Фамилия {i}-го студента: '),
-#             'Балл': int(input('Балл: '))
-#  } for i in range(1, students_count + 1)
-#     ]
-#
-#     return data
-#
-#
-# def get_average_grade(data: list[dict[str: Union[str, int]]]) -> Union[int, float]:
-#     """Функция подсчета среднего бала студентов."""
-#     grades: list[int] = [student['Балл'] for student in data]
-#     return round(sum(grades) / students_count)
-#
-#
-# def print_report(data: list[dict[str: Union[str, int]]]):
-#     """Функция вывода на печать запроса к базе данных."""
-#     average_grade: Union[int, float] = get_average_grade(data)
-#     print(f'Средний бал: {average_grade}. Студентов с балом выше среднего:')
-#     for student in data:
-#         if student['Балл'] > average_grade:
-#             print(student['Имя'])
-#
-#
-# if __name__ == '__main__':
-# #     # Авто тест.
-# #     # Глобальная переменная для хранения количества студентов.
-# #     students_count: int = 5
-# #     students: list[dict[str: Union[str, int]]] = [
-# #  {'id': 1, 'Имя': 'Игорь', 'Фамилия': 'Исаев', 'Балл': 12},
-# #  {'id': 2, 'Имя': 'Валентин', 'Фамилия': 'Киреев', 'Балл': 7},
-# #  {'id': 3, 'Имя': 'Виктор', 'Фамилия': 'Степанов', 'Балл': 4},
-# #  {'id': 4, 'Имя': 'Григорий', 'Фамилия': 'Иванов', 'Балл': 9},
-# #  {'id': 5, 'Имя': 'Дмитрий', 'Фамилия': 'Кузнецов', 'Балл': 6}
-# #     ]
-#
-#     # Для ручного заполнения.
-#     # Глобальная переменная для хранения количества студентов.
-#     students_count: int
-#     students = fill_data()
-#
-#     print_report(students)
 
-
-# 30.12.2023-------------------
+# 30.12.2023---------------------------------------------------------------------
 
 # def outer(who):
 #      a = 5
@@ -4549,3 +4495,149 @@
 # del droid3
 #
 # print("Численность роботов: ", Robot.k)
+
+
+# 18.02.2024-------------------------------------------------------------------------------------------
+
+# Задача она же и домашняя, нужно будет добавить в двух вариантах прорерти и гет и сет
+
+# class Account:
+#     rate_usd = 0.013
+#     rate_eur = 0.011
+#     suffix = 'RUB'
+#     suffix_usd = 'USD'
+#     suffix_eur = 'EUR'
+#
+#     def __init__(self, surname, num, percent, value=0):
+#         self.surname = surname
+#         self.num = num
+#         self.percent = percent
+#         self.value = value
+#         print(f"Счет #{self.num} принадлежащий {self.surname} был открыт.")
+#         print("*" * 50)
+#
+#     def __del__(self):
+#         print("*" * 50)
+#         print(f"Счет #{self.num} принадлежащий {self.surname} был закрыт.")
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f"{val} {Account.suffix} было успешно добавлено!")
+#         self.print_balance()
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.value, Account.rate_usd)
+#         print(f"Состояние счета: {usd_val} {Account.suffix_usd}")
+#
+#     def convert_to_eur(self):
+#         uer_val = Account.convert(self.value, Account.rate_eur)
+#         print(f"Состояние счёта: {uer_val} {Account.suffix_eur}")
+#
+#     def print_balance(self):
+#         print(f"Текущий баланс {self.value} {Account.suffix}")
+#
+#     def edit_owner(self, surname):
+#         self.surname = surname
+#
+#     def add_percent(self):
+#         self.value += self.value * self.percent
+#         print("Проценты были успешно начислены")
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.value:
+#             print(f"К сожалению у вас нет {val} {Account.suffix}")
+#         else:
+#             self.value -= val
+#             print(f"{val} {Account.suffix} было успешно снять!")
+#         self.print_balance()
+#
+#     def print_info(self):
+#         print('Информация о счете:')
+#         print("-" * 20)
+#         print(f"#{self.num}")
+#         print(f"Владелец: {self.surname}")
+#         self.print_balance()
+#         print(f"Проценты: {self.percent:.0%}")
+#         print("-" * 20)
+#
+#
+# acc = Account("Долгих", "12345", 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# Account.set_usd_rate(2)
+# Account.set_eur_rate(3)
+# print()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# acc.edit_owner("Дюма")
+# acc.print_info()
+# print()
+# acc.add_percent()
+# print()
+# acc.withdraw_money(100)
+# print()
+# acc.withdraw_money(3000)
+# print()
+# acc.add_money(5000)
+# print()
+# acc.withdraw_money(3000)
+
+
+# @property
+# def fio(self):
+#     return self.__fio
+#
+#
+# @fio.setter
+# def fio(self, fio):
+#     self.verify_fio(fio)
+#     self.__fio = fio
+#
+#
+# @property
+# def old(self):
+#     return self.__old
+#
+#
+# @old.setter
+# def old(self, old):
+#     self.verify_old(old)
+#     self.__old = old
+#
+#
+# @property
+# def password(self):
+#     return self.__password
+#
+#
+# @password.setter
+# def password(self, ps):
+#     self.verify_ps(ps)
+#     self.__password = ps
+#
+#
+# @property
+# def weight(self):
+#     return self.__weight
+#
+#
+# @weight.setter
+# def weight(self, w):
+#     self.verify_weight(w)
+#     self.__weight = w
+
+
