9f280b56e115330b67962578c440c2e39748da3a
Master-M <145254775+GrinenkoIvan@users.noreply.github.com>
2024-03-10 20:20:02 +0300

    Lesson 20.01.2024

--- 20.01.2024.py
+++ 20.01.2024.py
@@ -1,7 +1,8 @@
-_str = 'I am learning Python. hello, WORLD!'  # Исходная строка.
-start_range = _str.find('h')  # Находим индекс первого совпадения.
-end_range = _str.rfind('h')  # Находим индекс второго совпадения.
-not_reversed_part = _str[start_range + 1:end_range]  # Создаем строку из символов лежащих в найденном диапазоне.
-reversed_part = ''.join(reversed(not_reversed_part))  # Разворачиваем строку
-_str = _str.replace(not_reversed_part, reversed_part)  # Собираем строку путём замены подстрок.
-print(_str)
+line = 'I am learning Python. hello, WORLD!'
+str_new_1 = line.find('h')
+str_new_2 = line.rfind('h')
+str_new_3 = line[str_new_1 + 1:str_new_2]
+rev_new = ''.join(reversed(str_new_3))
+line_new = line.replace(str_new_3, rev_new)
+print(line_new)
+
